490dfa1e64a6e842b832e6bd924c0a662aa8788e
SaranSelva18 <git.with.saran@gmail.com>
2025-02-04 18:23:01 +0530

    Reversed a number

--- Add1.java
+++ Add1.java
@@ -4,7 +4,6 @@ public static void main(String[] args){
 Scanner sc = new Scanner(System.in);
 int a = sc.nextInt();
 int b = sc.nextInt();
-int c = a+b;
-System.out.println("Addition of two numbers : "+c);
+System.out.println("Addition of two numbers : "+ (a + b));
 }
 }
--- NegAddOrSun.java
+++ NegAddOrSun.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class NegAddOrS {
+public class NegAddOrSun {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
